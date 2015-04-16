424ebb8365591eff4c684d4dabc9c03990c8f6a4
M1lena <stoevami@gmail.com>
2015-04-16 10:27:19 +0100

    default temperature test failing

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,6 +1,8 @@
 describe("thermostat functionality", function() {
 
   it("has a default temperature of 20", function() {
+    thermostat = new Thermostat();
+    expect(thermostat.default_temp()).toEqual(20);
 
   });
 
