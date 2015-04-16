687ddb80ff08efe9cdc426d8cae7b68fc264daa7
M1lena <stoevami@gmail.com>
2015-04-16 10:57:27 +0100

    has a minimum temperature test passes

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,25 +1,30 @@
 describe("thermostat functionality", function() {
-  it("has a default temperature of 20", function() {
+
+  beforeEach(function() {
     thermostat = new Thermostat();
+  });
+
+  it("has a default temperature of 20", function() {
     expect(thermostat.default_temp).toEqual(20);
   });
 
   it('can increase the temperature', function(){
-    thermostat = new Thermostat();
     thermostat.up();
     expect(thermostat.default_temp).toEqual(21);
   });
 
   it('can decrease the temperature', function() {
-    thermostat = new Thermostat();
     thermostat.down();
     expect(thermostat.default_temp).toEqual(19);
   });
 
   it('has a minimum temperature of 10', function(){
-    thermostat = new Thermostat();
     thermostat.default_temp = 10;
     expect( function(){ thermostat.down(); } ).toThrow(new Error('10 is the minimum temp'));
+  });
+
+  it('has Power Saving mode that is automatically on', function() {
+    expect(thermostat.powerSaver).toEqual(true);
   });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,5 +1,6 @@
 var Thermostat = function(){
   this.default_temp = 20;
+  this.minimum_temp = 10;
 };
 
 Thermostat.prototype.up = function() {
@@ -7,5 +8,8 @@ Thermostat.prototype.up = function() {
 };
 
 Thermostat.prototype.down = function() {
+  if (this.default_temp === this.minimum_temp) {
+    throw new Error('10 is the minimum temp');
+  };
   this.default_temp --;
 };
